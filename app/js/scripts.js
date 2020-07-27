cf734ebe3d282b75a411facf3c05d57506309652
aaronpruzaniec <aaronpruzaniec@gmail.com>
2020-07-26 21:01:38 -0400

    init

---
+++ app/js/scripts.js
@@ -0,0 +1,486 @@
+console.log("tttt");
+let data = {
+  version: "1.0",
+  encoding: "UTF-8",
+  feed: {
+    xmlns: "http://www.w3.org/2005/Atom",
+    xmlns$openSearch: "http://a9.com/-/spec/opensearchrss/1.0/",
+    xmlns$gsx: "http://schemas.google.com/spreadsheets/2006/extended",
+    id: {
+      $t:
+        "https://spreadsheets.google.com/feeds/list/1kiJGHjmgYfCS3ZCXF1J2fVV67WdoH9lwqp-1A-7XtCQ/od6/public/values",
+    },
+    updated: { $t: "2020-07-21T01:49:01.573Z" },
+    category: [
+      {
+        scheme: "http://schemas.google.com/spreadsheets/2006",
+        term: "http://schemas.google.com/spreadsheets/2006#list",
+      },
+    ],
+    title: { type: "text", $t: "Sheet1" },
+    link: [
+      {
+        rel: "alternate",
+        type: "application/atom+xml",
+        href:
+          "https://docs.google.com/spreadsheets/d/1kiJGHjmgYfCS3ZCXF1J2fVV67WdoH9lwqp-1A-7XtCQ/pubhtml",
+      },
+      {
+        rel: "http://schemas.google.com/g/2005#feed",
+        type: "application/atom+xml",
+        href:
+          "https://spreadsheets.google.com/feeds/list/1kiJGHjmgYfCS3ZCXF1J2fVV67WdoH9lwqp-1A-7XtCQ/od6/public/values",
+      },
+      {
+        rel: "http://schemas.google.com/g/2005#post",
+        type: "application/atom+xml",
+        href:
+          "https://spreadsheets.google.com/feeds/list/1kiJGHjmgYfCS3ZCXF1J2fVV67WdoH9lwqp-1A-7XtCQ/od6/public/values",
+      },
+      {
+        rel: "self",
+        type: "application/atom+xml",
+        href:
+          "https://spreadsheets.google.com/feeds/list/1kiJGHjmgYfCS3ZCXF1J2fVV67WdoH9lwqp-1A-7XtCQ/od6/public/values?alt\u003djson",
+      },
+    ],
+    author: [{ name: { $t: "bigpru" }, email: { $t: "bigpru@gmail.com" } }],
+    openSearch$totalResults: { $t: "12" },
+    openSearch$startIndex: { $t: "1" },
+    entry: [
+      {
+        id: {
+          $t:
+            "https://spreadsheets.google.com/feeds/list/1kiJGHjmgYfCS3ZCXF1J2fVV67WdoH9lwqp-1A-7XtCQ/od6/public/values/cokwr",
+        },
+        updated: { $t: "2020-07-21T01:49:01.573Z" },
+        category: [
+          {
+            scheme: "http://schemas.google.com/spreadsheets/2006",
+            term: "http://schemas.google.com/spreadsheets/2006#list",
+          },
+        ],
+        title: { type: "text", $t: "Samantha Apgar" },
+        content: {
+          type: "text",
+          $t:
+            "title: Secretary, image: //lwvphilly.org/wp-content/uploads/2019/09/Apgar-Headshot-e1569287678838-1024x1024.jpg",
+        },
+        link: [
+          {
+            rel: "self",
+            type: "application/atom+xml",
+            href:
+              "https://spreadsheets.google.com/feeds/list/1kiJGHjmgYfCS3ZCXF1J2fVV67WdoH9lwqp-1A-7XtCQ/od6/public/values/cokwr",
+          },
+        ],
+        gsx$name: { $t: "Samantha Apgar" },
+        gsx$title: { $t: "Secretary" },
+        gsx$image: {
+          $t:
+            "//lwvphilly.org/wp-content/uploads/2019/09/Apgar-Headshot-e1569287678838-1024x1024.jpg",
+        },
+      },
+      {
+        id: {
+          $t:
+            "https://spreadsheets.google.com/feeds/list/1kiJGHjmgYfCS3ZCXF1J2fVV67WdoH9lwqp-1A-7XtCQ/od6/public/values/cpzh4",
+        },
+        updated: { $t: "2020-07-21T01:49:01.573Z" },
+        category: [
+          {
+            scheme: "http://schemas.google.com/spreadsheets/2006",
+            term: "http://schemas.google.com/spreadsheets/2006#list",
+          },
+        ],
+        title: { type: "text", $t: "Shannon Connell-Robichaud" },
+        content: {
+          type: "text",
+          $t:
+            "title: Communications Chair, image: //lwvphilly.org/wp-content/uploads/2019/12/Shannon-Connell-e1575764757364.jpg",
+        },
+        link: [
+          {
+            rel: "self",
+            type: "application/atom+xml",
+            href:
+              "https://spreadsheets.google.com/feeds/list/1kiJGHjmgYfCS3ZCXF1J2fVV67WdoH9lwqp-1A-7XtCQ/od6/public/values/cpzh4",
+          },
+        ],
+        gsx$name: { $t: "Shannon Connell-Robichaud" },
+        gsx$title: { $t: "Communications Chair" },
+        gsx$image: {
+          $t:
+            "//lwvphilly.org/wp-content/uploads/2019/12/Shannon-Connell-e1575764757364.jpg",
+        },
+      },
+      {
+        id: {
+          $t:
+            "https://spreadsheets.google.com/feeds/list/1kiJGHjmgYfCS3ZCXF1J2fVV67WdoH9lwqp-1A-7XtCQ/od6/public/values/cre1l",
+        },
+        updated: { $t: "2020-07-21T01:49:01.573Z" },
+        category: [
+          {
+            scheme: "http://schemas.google.com/spreadsheets/2006",
+            term: "http://schemas.google.com/spreadsheets/2006#list",
+          },
+        ],
+        title: { type: "text", $t: "Lauren Cristella" },
+        content: {
+          type: "text",
+          $t:
+            "title: President, image: //lwvphilly.org/wp-content/uploads/2019/09/LC-Headshot-e1569288382797-1024x1024.jpg",
+        },
+        link: [
+          {
+            rel: "self",
+            type: "application/atom+xml",
+            href:
+              "https://spreadsheets.google.com/feeds/list/1kiJGHjmgYfCS3ZCXF1J2fVV67WdoH9lwqp-1A-7XtCQ/od6/public/values/cre1l",
+          },
+        ],
+        gsx$name: { $t: "Lauren Cristella" },
+        gsx$title: { $t: "President" },
+        gsx$image: {
+          $t:
+            "//lwvphilly.org/wp-content/uploads/2019/09/LC-Headshot-e1569288382797-1024x1024.jpg",
+        },
+      },
+      {
+        id: {
+          $t:
+            "https://spreadsheets.google.com/feeds/list/1kiJGHjmgYfCS3ZCXF1J2fVV67WdoH9lwqp-1A-7XtCQ/od6/public/values/chk2m",
+        },
+        updated: { $t: "2020-07-21T01:49:01.573Z" },
+        category: [
+          {
+            scheme: "http://schemas.google.com/spreadsheets/2006",
+            term: "http://schemas.google.com/spreadsheets/2006#list",
+          },
+        ],
+        title: { type: "text", $t: "Samantha Kelly" },
+        content: {
+          type: "text",
+          $t:
+            "title: Membership Chair, image: //lwvphilly.org/wp-content/uploads/2019/11/Kelly-Headshot-e1574046549997.jpg",
+        },
+        link: [
+          {
+            rel: "self",
+            type: "application/atom+xml",
+            href:
+              "https://spreadsheets.google.com/feeds/list/1kiJGHjmgYfCS3ZCXF1J2fVV67WdoH9lwqp-1A-7XtCQ/od6/public/values/chk2m",
+          },
+        ],
+        gsx$name: { $t: "Samantha Kelly" },
+        gsx$title: { $t: "Membership Chair" },
+        gsx$image: {
+          $t:
+            "//lwvphilly.org/wp-content/uploads/2019/11/Kelly-Headshot-e1574046549997.jpg",
+        },
+      },
+      {
+        id: {
+          $t:
+            "https://spreadsheets.google.com/feeds/list/1kiJGHjmgYfCS3ZCXF1J2fVV67WdoH9lwqp-1A-7XtCQ/od6/public/values/ciyn3",
+        },
+        updated: { $t: "2020-07-21T01:49:01.573Z" },
+        category: [
+          {
+            scheme: "http://schemas.google.com/spreadsheets/2006",
+            term: "http://schemas.google.com/spreadsheets/2006#list",
+          },
+        ],
+        title: { type: "text", $t: "Georgia Mae Lively" },
+        content: {
+          type: "text",
+          $t:
+            "title: Board Member, image: //lwvphilly.org/wp-content/uploads/2019/12/Georgia-Mae-Lively.jpg",
+        },
+        link: [
+          {
+            rel: "self",
+            type: "application/atom+xml",
+            href:
+              "https://spreadsheets.google.com/feeds/list/1kiJGHjmgYfCS3ZCXF1J2fVV67WdoH9lwqp-1A-7XtCQ/od6/public/values/ciyn3",
+          },
+        ],
+        gsx$name: { $t: "Georgia Mae Lively" },
+        gsx$title: { $t: "Board Member" },
+        gsx$image: {
+          $t:
+            "//lwvphilly.org/wp-content/uploads/2019/12/Georgia-Mae-Lively.jpg",
+        },
+      },
+      {
+        id: {
+          $t:
+            "https://spreadsheets.google.com/feeds/list/1kiJGHjmgYfCS3ZCXF1J2fVV67WdoH9lwqp-1A-7XtCQ/od6/public/values/ckd7g",
+        },
+        updated: { $t: "2020-07-21T01:49:01.573Z" },
+        category: [
+          {
+            scheme: "http://schemas.google.com/spreadsheets/2006",
+            term: "http://schemas.google.com/spreadsheets/2006#list",
+          },
+        ],
+        title: { type: "text", $t: "Natalie Parker" },
+        content: {
+          type: "text",
+          $t:
+            "title: Voter Services Chair, image: //lwvphilly.org/wp-content/uploads/2019/09/Parker-Headshot-e1569290207403-1024x1024.jpg",
+        },
+        link: [
+          {
+            rel: "self",
+            type: "application/atom+xml",
+            href:
+              "https://spreadsheets.google.com/feeds/list/1kiJGHjmgYfCS3ZCXF1J2fVV67WdoH9lwqp-1A-7XtCQ/od6/public/values/ckd7g",
+          },
+        ],
+        gsx$name: { $t: "Natalie Parker" },
+        gsx$title: { $t: "Voter Services Chair" },
+        gsx$image: {
+          $t:
+            "//lwvphilly.org/wp-content/uploads/2019/09/Parker-Headshot-e1569290207403-1024x1024.jpg",
+        },
+      },
+      {
+        id: {
+          $t:
+            "https://spreadsheets.google.com/feeds/list/1kiJGHjmgYfCS3ZCXF1J2fVV67WdoH9lwqp-1A-7XtCQ/od6/public/values/clrrx",
+        },
+        updated: { $t: "2020-07-21T01:49:01.573Z" },
+        category: [
+          {
+            scheme: "http://schemas.google.com/spreadsheets/2006",
+            term: "http://schemas.google.com/spreadsheets/2006#list",
+          },
+        ],
+        title: { type: "text", $t: "Kerry Sautner" },
+        content: {
+          type: "text",
+          $t:
+            "title: Vice Chair, image: //lwvphilly.org/wp-content/uploads/2019/10/Sautner-Headshot-e1572155001672.jpg",
+        },
+        link: [
+          {
+            rel: "self",
+            type: "application/atom+xml",
+            href:
+              "https://spreadsheets.google.com/feeds/list/1kiJGHjmgYfCS3ZCXF1J2fVV67WdoH9lwqp-1A-7XtCQ/od6/public/values/clrrx",
+          },
+        ],
+        gsx$name: { $t: "Kerry Sautner" },
+        gsx$title: { $t: "Vice Chair" },
+        gsx$image: {
+          $t:
+            "//lwvphilly.org/wp-content/uploads/2019/10/Sautner-Headshot-e1572155001672.jpg",
+        },
+      },
+      {
+        id: {
+          $t:
+            "https://spreadsheets.google.com/feeds/list/1kiJGHjmgYfCS3ZCXF1J2fVV67WdoH9lwqp-1A-7XtCQ/od6/public/values/cyevm",
+        },
+        updated: { $t: "2020-07-21T01:49:01.573Z" },
+        category: [
+          {
+            scheme: "http://schemas.google.com/spreadsheets/2006",
+            term: "http://schemas.google.com/spreadsheets/2006#list",
+          },
+        ],
+        title: { type: "text", $t: "Ashley Session" },
+        content: {
+          type: "text",
+          $t:
+            "title: Policy Commitee Co-Chair, image: //lwvphilly.org/wp-content/uploads/2019/09/Session-Headshot-e1569290334900-1024x1024.jpg",
+        },
+        link: [
+          {
+            rel: "self",
+            type: "application/atom+xml",
+            href:
+              "https://spreadsheets.google.com/feeds/list/1kiJGHjmgYfCS3ZCXF1J2fVV67WdoH9lwqp-1A-7XtCQ/od6/public/values/cyevm",
+          },
+        ],
+        gsx$name: { $t: "Ashley Session" },
+        gsx$title: { $t: "Policy Commitee Co-Chair" },
+        gsx$image: {
+          $t:
+            "//lwvphilly.org/wp-content/uploads/2019/09/Session-Headshot-e1569290334900-1024x1024.jpg",
+        },
+      },
+      {
+        id: {
+          $t:
+            "https://spreadsheets.google.com/feeds/list/1kiJGHjmgYfCS3ZCXF1J2fVV67WdoH9lwqp-1A-7XtCQ/od6/public/values/cztg3",
+        },
+        updated: { $t: "2020-07-21T01:49:01.573Z" },
+        category: [
+          {
+            scheme: "http://schemas.google.com/spreadsheets/2006",
+            term: "http://schemas.google.com/spreadsheets/2006#list",
+          },
+        ],
+        title: { type: "text", $t: "Elizabeth Tinker" },
+        content: {
+          type: "text",
+          $t:
+            "title: Policy Commitee Co-Chair, image: //lwvphilly.org/wp-content/uploads/2019/11/Tinker-headshot-scaled-e1574045805411.jpg",
+        },
+        link: [
+          {
+            rel: "self",
+            type: "application/atom+xml",
+            href:
+              "https://spreadsheets.google.com/feeds/list/1kiJGHjmgYfCS3ZCXF1J2fVV67WdoH9lwqp-1A-7XtCQ/od6/public/values/cztg3",
+          },
+        ],
+        gsx$name: { $t: "Elizabeth Tinker" },
+        gsx$title: { $t: "Policy Commitee Co-Chair" },
+        gsx$image: {
+          $t:
+            "//lwvphilly.org/wp-content/uploads/2019/11/Tinker-headshot-scaled-e1574045805411.jpg",
+        },
+      },
+      {
+        id: {
+          $t:
+            "https://spreadsheets.google.com/feeds/list/1kiJGHjmgYfCS3ZCXF1J2fVV67WdoH9lwqp-1A-7XtCQ/od6/public/values/d180g",
+        },
+        updated: { $t: "2020-07-21T01:49:01.573Z" },
+        category: [
+          {
+            scheme: "http://schemas.google.com/spreadsheets/2006",
+            term: "http://schemas.google.com/spreadsheets/2006#list",
+          },
+        ],
+        title: { type: "text", $t: "Cassie Tomkins" },
+        content: {
+          type: "text",
+          $t:
+            "title: Board Member, image: //lwvphilly.org/wp-content/uploads/2019/10/Cassie-Tomkins-Headshot-e1572154935910.jpg",
+        },
+        link: [
+          {
+            rel: "self",
+            type: "application/atom+xml",
+            href:
+              "https://spreadsheets.google.com/feeds/list/1kiJGHjmgYfCS3ZCXF1J2fVV67WdoH9lwqp-1A-7XtCQ/od6/public/values/d180g",
+          },
+        ],
+        gsx$name: { $t: "Cassie Tomkins" },
+        gsx$title: { $t: "Board Member" },
+        gsx$image: {
+          $t:
+            "//lwvphilly.org/wp-content/uploads/2019/10/Cassie-Tomkins-Headshot-e1572154935910.jpg",
+        },
+      },
+      {
+        id: {
+          $t:
+            "https://spreadsheets.google.com/feeds/list/1kiJGHjmgYfCS3ZCXF1J2fVV67WdoH9lwqp-1A-7XtCQ/od6/public/values/d2mkx",
+        },
+        updated: { $t: "2020-07-21T01:49:01.573Z" },
+        category: [
+          {
+            scheme: "http://schemas.google.com/spreadsheets/2006",
+            term: "http://schemas.google.com/spreadsheets/2006#list",
+          },
+        ],
+        title: { type: "text", $t: "Melissa Wright" },
+        content: {
+          type: "text",
+          $t:
+            "title: Treasurer, image: //lwvphilly.org/wp-content/uploads/2019/09/Wright-Headshot-e1569293532624-1024x1024.jpg",
+        },
+        link: [
+          {
+            rel: "self",
+            type: "application/atom+xml",
+            href:
+              "https://spreadsheets.google.com/feeds/list/1kiJGHjmgYfCS3ZCXF1J2fVV67WdoH9lwqp-1A-7XtCQ/od6/public/values/d2mkx",
+          },
+        ],
+        gsx$name: { $t: "Melissa Wright" },
+        gsx$title: { $t: "Treasurer" },
+        gsx$image: {
+          $t:
+            "//lwvphilly.org/wp-content/uploads/2019/09/Wright-Headshot-e1569293532624-1024x1024.jpg",
+        },
+      },
+    ],
+  },
+};
+let members = data.feed.entry,
+  temp = {};
+
+members = members.map((x) => {
+  temp[x.gsx$name.$t] = {
+    title: x.gsx$title.$t,
+    image: x.gsx$image.$t,
+  };
+  console.log(x);
+});
+
+// members = members.map((x) => [
+//   {
+//     name: x.gsx$name.$t,
+//     title: x.gsx$title.$t,
+//     image: x.gsx$image.$t,
+//   },
+// ]);
+
+//[x.gsx$name.$t, x.gsx$title.$t, x.gsx$image.$t]
+
+let h2 = document.createElement("h4");
+let image = document.createElement("img");
+let sample = Object.entries(document.querySelectorAll("h2"));
+let boardMatrixContent = "";
+
+sample.map((x) => {
+  let hyphenated = x[1].innerText.replace(" ", "-"),
+    name = x[1].innerText,
+    title = temp[x[1].innerText].title,
+    image = temp[x[1].innerText].image;
+  x[1].outerHTML = `
+  <div class="board-member" id="${hyphenated}">
+    <h2>${name}</h2>
+    <h3>${title}</h3>
+    <img src="${image}">
+  </div>`;
+  boardMatrix = `
+  <ul class="wp-block-gallery columns-3 is-cropped">
+  <li class="blocks-gallery-item">
+    <figure><img src="${image} 1100w" sizes="(max-width: 1024px) 100vw, 1024px">
+      <figcaption><a href="#${hyphenated}">#${hyphenated}</a></figcaption>
+    </figure>
+  </li>
+</ul>
+  `;
+  boardMatrixContent += boardMatrix;
+
+  //x[1].parentNode.replaceChild(outer2, x[1]);
+  //x[1].insertBefore(outer2, x[1]);
+  //console.log(temp[x[1].innerText]);
+  // x[1].insertBefore(title);
+  // return (x[1].textContent = "hello");
+});
+
+Object.entries(temp).map((x) => {
+  console.log(x);
+});
+
+document.querySelectorAll(".entry-content")[0].innerHTML =
+  boardMatrixContent + document.querySelectorAll(".entry-content")[0].innerHTML;
+
+//   sample.map((x) => {
+//   x[1].textContent = "hello";
+//   x[1].innerHTML += title;
+//   // x[1].insertBefore(title);
+//   // return (x[1].textContent = "hello");
+// });
+
+console.log(content);
